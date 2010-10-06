5d4cb1b7b13569c8048324656c805a0f434da0a4
Freenerd <johan@soundcloud.com>
2010-10-06 18:01:25 +0200

    Channels in Socky

--- app/controllers/socky_controller.rb
+++ app/controllers/socky_controller.rb
@@ -12,51 +12,55 @@ class SockyController < ApplicationController
     render :text => "ok"
   end
 
-  #def unsubscribe
-    #Socky.send ({:action => "unsubscribe",
-                 #:channel => params[:channel],
-                 #:user => (params[:client_id] || '')}.to_json)
-    #render :text => "ok"
-  #end
+  def unsubscribe
+    Socky.send ({:action => "unsubscribe",
+                 :channel => params[:channel],
+                 :user => (params[:client_id] || '')}.to_json)
+    render :text => "ok"
+  end
 
   # Performed when user has chosen username
   def login
     Socky.send ({:action  => "login", 
                  :user    => params[:user],
-                 :channel => params[:channel]}.to_json)
+                 :channel => params[:channel]}.to_json,
+               :to => { :channels => params[:channel] })
   render :text => "ok"
   end
 
   def logout
-    Socky.send ({:action  => "logout", 
+    Socky.send({:action  => "logout", 
                  :user    => params[:user],
-                 :channel => params[:channel]}.to_json)
+                 :channel => params[:channel]}.to_json,
+               :to => { :channels => params[:channel] })
   render :text => "ok"
   end
 
 
   def message
-    Socky.send ({:action  => "message", 
+    Socky.send( {:action  => "message",
                  :client  => (params[:client_id] || ''),
                  :user    => params[:user],
                  :channel => params[:channel],
-                 :message => params[:message]}.to_json)
+                 :message => params[:message]}.to_json,
+                :to => { :channels => params[:channel] })
     render :text => "ok"
   end
 
   def start
-    puts params;
     Socky.send ({:action  => "start", 
                  :channel => params[:channel],
                  :client  => (params[:client_id] || ''),
-                 :postion => (params[:position] || '')}.to_json)
+                 :postion => (params[:position] || '')}.to_json,
+                :to => { :channels => params[:channel] })
     render :text => "ok"
   end
 
   def pause
     Socky.send ({:action => "pause",
                  :channel => params[:channel],
-                 :client => (params[:client_id] || '')}.to_json)
+                 :client => (params[:client_id] || '')}.to_json,
+                :to => { :channels => params[:channel] })
     render :text => "ok"
   end
 
